8a9da08f32723b0f9a6d59fe54a45ec285224552
Ramanajee Kandregula <ramanajee516@live.in>
2015-12-04 09:03:37 +0530

    BackboneDemo

---
+++ BackboneDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BackboneDemo.Startup))]
+namespace BackboneDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
